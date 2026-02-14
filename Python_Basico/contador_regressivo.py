76ae3cd9cd683eaa44e80433be540b5ef8f580d0
Bruno de Oliveira Sutil <brunosutil30@gmail.com>
2026-02-13 22:09:49 -0300

    docs: Adiciona comentário da desrição do projeto com etapas.

--- Python_Basico/contador_regressivo.py
+++ Python_Basico/contador_regressivo.py
@@ -1,3 +1,12 @@
+"""
+Docstring for Contador_Regressivo
+
+Projeto: Contador Regressivo
+ - Solicitar um número inicial ao usuário.
+ - Realizar uma contagem regressiva até zero.
+ - Exibir cada número da contagem.
+"""
+
 num = int(input("Digite um numero para decrementar: "))
 
 for num in range(num, 0-1, -1):
